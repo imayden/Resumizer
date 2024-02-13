0c748cac7c1de5f9ad6b06cb27021e3574d5dc5f
phhoang98a <hphan11@depaul.edu>
2024-02-13 17:40:46 -0600

    update Job Api

--- backend/JobServer/app.py
+++ backend/JobServer/app.py
@@ -1,20 +1,23 @@
-from flask import Flask, request
+from pydantic import BaseModel
 from jobspy import scrape_jobs
+from fastapi import FastAPI
 import pandas as pd
-app = Flask(__name__)
-@app.route("/hello")
-def hello():
-  return "hello", 200
 
-@app.route("/job", methods=['POST'])
-def job():
-  job_title = request.json['job_title']
-  country = request.json['country']
-  location = request.json['location']
+app = FastAPI()
+
+class Job(BaseModel):
+  job_title: str
+  country: str
+  location: str = None
+
+@app.post("/jobs/")
+async def find_job(job:Job):
+  job_title = job.job_title
+  country = job.country
+  location = job.location
 
   jobs: pd.DataFrame = scrape_jobs(
-      # site_name=["indeed", "linkedin", "glassdoor"],
-      site_name=["indeed"],
+      site_name=["indeed", "linkedin", "glassdoor"],
       search_term=job_title,
       location=location,
       results_wanted=10,
@@ -27,7 +30,4 @@ def job():
     "company": jobs["company"].tolist(),
     "location": jobs["location"].tolist(),
     "date_posted": jobs["date_posted"].tolist(),
-  }, 200
-
-if __name__ == "__name__":
-  app.run()
+  }
