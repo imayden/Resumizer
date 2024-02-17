628cb2ce9512c5cefa4877ffe42ee08a183131c5
Danny Capua <danielcapua19@gmail.com>
2024-02-16 22:50:25 -0600

    finish form UI and backend call

--- frontend/src/components/JobSpy.jsx
+++ frontend/src/components/JobSpy.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const JobSpy = () => {
+function JobSpy() {
+
+  const [jobData, setJobData] = useState({
+    job_title: '',
+    country: '',
+    location: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setJobData({
+      ...jobData,
+      [name]: value
+    });
+  };
+
+  // jobspy POST
+  const handleSearch = (event) => {
+    event.preventDefault();
+    axios.post('https://job-server-0wyb.onrender.com/jobs', jobData)
+      .then(response => {
+        console.log('Job search successful:', response.data);
+
+        setJobData({ // reset
+          job_title: '',
+          country: '',
+          location: ''
+        });
+      })
+      .catch(error => {
+        console.error('Error searching for job:', error);
+      });
+  };
+
   return (
     <div
       className="flex w-full flex-col items-start gap-x-8 gap-y-8 px-12 py-10 rounded-3xl max-mdd:max-w-none max-md:p-8 mb-[8px]
@@ -13,37 +47,55 @@ const JobSpy = () => {
 
       <div style={{ display: "flex", gap: "10px" }}>
         <div>
-          <form>
+          <form onSubmit={handleSearch}>
             <input
-              className="min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
-              placeholder="Enter job title"
+              className = "min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
+              type = "text"
+              name = "job_title"
+              value = {jobData.job_title}
+              onChange = {handleChange}
+              placeholder = "Enter job title"
+              required
             ></input>
           </form>
         </div>
         <div>
           <form>
             <input
-              className="min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
-              placeholder="Enter country"
+              className = "min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
+              type = "text"
+              name = "country"
+              value = {jobData.country}
+              onChange = {handleChange}
+              placeholder = "Enter country"
+              required
             ></input>
           </form>
         </div>
         <div>
           <form>
             <input
-              className="min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
-              placeholder="Enter location"
+              className = "min-h-[96px] transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-4 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
+              type = "text"
+              name = "location"
+              value = {jobData.location}
+              onChange = {handleChange}
+              placeholder = "Enter location"
+              required
             ></input>
           </form>
         </div>
         <div>
-          <button className="min-h-[96px] w-full transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-8 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] ">
-            Search
+          <button
+            className = "min-h-[96px] w-full transition-[background-color] duration-300 ease-[ease-out] text-[28px] leading-[48px] font-medium text-center tracking-[-0.01em] px-8 py-6 rounded-[99px] max-md:min-h-[80px] max-md:text-2xl max-md:leading-8 text-white bg-[#4F0ED1] hover:bg-[#6D49FE] "
+            type = "submit"
+          >
+            Search Jobs
           </button>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default JobSpy;
