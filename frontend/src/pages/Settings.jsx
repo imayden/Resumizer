162b702f0446797a368715b5ed4f4dd76f2fa833
imayden <ayden.yiming.deng@gmail.com>
2024-02-26 11:45:05 -0600

    update

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -6,6 +6,7 @@ import Container from '../components/Container';
 import Button from '../components/Button';
 import Tile from '../components/Tile';
 import InputField from '../components/InputField';
+import Socials from '../components/Socials';
 
 const Setting = () => {
     return (
@@ -34,6 +35,10 @@ const Setting = () => {
                     </Button>
                 </Tile>
 
+            </FadeIn>
+
+            <FadeIn>
+                <Socials />
             </FadeIn>
         </Container>
     );
